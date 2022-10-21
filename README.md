e49fbf534d515071520f264e8dc5816dcf8afbc9
Your Name <you@example.com>
2022-10-21 14:07:50 +0800

    feat: README and cli

---
+++ README.md
@@ -0,0 +1,7 @@
+# Usage
+
+git-explore list -d "d:\\rust\\backend\\sdk1018"
+git-explore init -d "d:\\rust\\backend\\sdk1018"
+> create a file git-explore.db with maxvalue of cargo.toml
+git-explore commit -d "d:\\rust\\backend\\sdk1018"
+> change version on git-explore.db and Cargo.toml
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use clap::Parser;
 use git_explore::*;
 
 fn main() {
-    let cli = RepoCli::parse_from([KEY_COMMAND, "list", "-d", "d:\\rust\\backend\\sdk1018"]);
+    let cli = RepoCli::parse();
+    // let cli = RepoCli::parse_from([KEY_COMMAND, "list", "-d", "d:\\rust\\backend\\sdk1018"]);
     // println!("cli: {:#?}", cli);
     match cli.command {
         Some(Command::List(opt)) => run_list(&opt).unwrap(),
